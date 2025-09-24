98e186226b5ddc4a3e4b1cc83ebc0f07e742b28c
Peak-Worth <hrushikesha.crimson@gmail.com>
2025-09-24 17:59:33 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,10 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-
+import solar_theme
 # -- Options for HTML output
 html_theme = 'solar-theme'
+html_theme_path = [solar_theme.theme_path]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
