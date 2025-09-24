ad699750c5f99b9321f085b2efcee18820eb5e70
Peak-Worth <hrushikesha.crimson@gmail.com>
2025-09-24 17:21:23 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,8 +28,6 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
-# -- html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_pdj_theme'
 
 # -- Options for EPUB output
